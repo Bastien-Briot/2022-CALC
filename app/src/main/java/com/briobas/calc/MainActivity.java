cbb0817d24a030650ba66211219533b39c0fb0fe
briobas <bastien.briot@divtec.ch>
2022-02-08 08:13:06 +0100

    Première version finit

--- app/src/main/java/com/briobas/calc/MainActivity.java
+++ app/src/main/java/com/briobas/calc/MainActivity.java
@@ -1,11 +1,13 @@
 package com.briobas.calc;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,115 +19,80 @@ public class MainActivity extends AppCompatActivity {
         DOT("."),
         POURCENT("%");
 
-        private String name = "";
-
+        private final String name;
         Ops(String name) {
             this.name = name;
         }
-
+        @NonNull
         public String toString() {
             return name;
         }
     }
-
-
     private TextView screen;
     private double op1 = 0;
     private double op2 = 0;
     private Ops operator = null;
     private boolean isOp1 = true;
-    private Button BT_Point;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button BT_Point = findViewById(R.id.BTN_Virgule);
-        screen = (TextView) findViewById(R.id.resultTXT);
-        Button btnEgal = (Button) findViewById(R.id.BTN_Egal);
-        btnEgal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    Double s = Double.parseDouble(screen.getText().toString());
-                    String[] sheesh = String.valueOf(s).split("\\+");
-                    Double[] doubleArr = new Double[2];
-                    op1 = doubleArr[0] = Double.parseDouble(sheesh[0]);
-                    op2 = doubleArr[1] = Double.parseDouble(sheesh[1]);
-                    Log.v("Nombre1", String.valueOf(op1));
-                    Log.v("Nombre2", String.valueOf(op2));
-                    compute();
-                    BT_Point.setEnabled(true);
-                } catch(NumberFormatException | ClassCastException e) {
-                    Log.v("error: ", String.valueOf(e));
-                }
-
-            }
-        });
-
-        Button BT_Clear = findViewById(R.id.BTN_Reset);
-        BT_Clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clear();
+        screen = findViewById(R.id.resultTXT);
+        Button btnEgal = findViewById(R.id.BTN_Egal);
+        btnEgal.setOnClickListener(v -> {
+            String s = screen.getText().toString();
+            String[] valueString = s.split("[+/%\\-*]");
+            if (valueString[1] == null) { screen.setText("Error"); }
+            if (valueString[0].equals(".") || valueString[1].equals(".")) { screen.setText("Error"); } else {
+                op1 = Double.parseDouble(valueString[0]);
+                op2 = Double.parseDouble(valueString[1]);
+                Log.v("Nombre1", String.valueOf(op1));
+                Log.v("Nombre2", String.valueOf(op2));
+                compute();
                 BT_Point.setEnabled(true);
             }
-        });
 
-        Button BT_Pourcent = findViewById(R.id.BTN_Pourcent);
-        BT_Pourcent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                operator = Ops.POURCENT;
-                compute();
-            }
-        });
-        BT_Point.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                screen.append(".");
-                BT_Point.setEnabled(false);
-            }
-        });
+    });
 
-    }
+    Button BT_Clear = findViewById(R.id.BTN_Reset);
+        BT_Clear.setOnClickListener(view -> {
+        clear();
+        BT_Point.setEnabled(true);
+    });
 
-    private void updateDisplay() {
-        Button BT_Point = findViewById(R.id.BTN_Virgule);
-        double v = op1;
-        if (!isOp1) {
-            v = op2;
-        }
-        screen.append(String.valueOf(String.format("%.0f", v)));
-    }
+    Button BT_Pourcent = findViewById(R.id.BTN_Pourcent);
+        BT_Pourcent.setOnClickListener(view -> {
+        operator = Ops.POURCENT;
+        compute();
+    });
+        BT_Point.setOnClickListener(view -> {
+        screen.append(".");
+        BT_Point.setEnabled(false);
+    });
 
+}
 
     public void compute() {
         if (isOp1) {
             if (operator == Ops.POURCENT) {
-                screen.setText(String.valueOf(op1 /= 100));
+                double val = Double.parseDouble(screen.getText().toString());
+                double valpourcent = val/100;
+                screen.setText(String.valueOf(valpourcent));
             }
         } else {
             switch (operator) {
-                case PLUS:
-                    op1 += op2;
-                    break;
-                case MOINS:
-                    op1 -= op2;
-                    break;
-                case FOIS:
-                    op1 *= op2;
-                    break;
-                case DIV:
-                    op1 /= op2;
-                    break;
-
-                default:
-                    return;
+                case PLUS: op1 += op2; break;
+                case MOINS: op1 -= op2; break;
+                case FOIS: op1 *= op2; break;
+                case DIV: op1 /= op2; break;
+                default: return;
             }
             op2 = 0;
             isOp1 = true;
-            updateDisplay();
+            screen.setText(String.valueOf(op1));
         }
     }
 
@@ -135,9 +102,10 @@ public class MainActivity extends AppCompatActivity {
         op2 = 0;
         operator = null;
         isOp1 = true;
-        updateDisplay();
+        screen.setText("");
     }
 
+    @SuppressLint("NonConstantResourceId")
     public void setOperator(View v) {
         switch (v.getId()) {
             case R.id.BTN_Plus:
@@ -173,15 +141,9 @@ public class MainActivity extends AppCompatActivity {
     public void addNumber(View v) {
         try {
             int val = Integer.parseInt(((Button) v).getText().toString());
-
-            if (isOp1) {
-                op1 = op1 * 10 + val;
-                updateDisplay();
-            } else {
-                op2 = op2 * 10 + val;
-                updateDisplay();
-            }
+            screen.append(String.valueOf(val));
         } catch (NumberFormatException | ClassCastException e) {
+            Log.v("Erreur: ", String.valueOf(e));
         }
     }
 
